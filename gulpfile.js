35ed97e4625e1a239a07de117748278cc1c8175d
Joseph Rosenberg <jofus101@gmail.com>
2015-03-14 18:07:56 -0400

    Beef up gulpfile.  Notifications don't work because of vagrant....

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
+// http://markgoodyear.com/2014/01/getting-started-with-gulp/
+// https://github.com/google/web-starter-kit/blob/master/gulpfile.js
+// http://www.justinmccandless.com/blog/A+Tutorial+for+Getting+Started+with+Gulp
+
 var gulp = require('gulp'),
-    sass = require('gulp-ruby-sass'),
+//    sass = require('gulp-ruby-sass'),
+    sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     minifycss = require('gulp-minify-css'),
     jshint = require('gulp-jshint'),
@@ -11,8 +16,61 @@ var gulp = require('gulp'),
     cache = require('gulp-cache'),
     livereload = require('gulp-livereload'),
     del = require('del');
+//    browserSync = require('browser-sync'),
+//    reload = browserSync.reload;
 
+// copy the html files
+gulp.task('copy', function() {
+  return gulp.src('src/index.html')
+  .pipe(gulp.dest('dist/'))
+});
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('styles', function() {
+  return gulp.src('src/styles/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('src/scripts/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/assets/js'))
+    .pipe(notify({ message: 'Scripts task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+  gulp.start('copy', 'styles', 'scripts');
+});
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('src/styles/**/*.scss', ['styles']);
+
+  // Watch .js files
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+
+  // Watch image files
+  gulp.watch('src/images/**/*', ['images']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
 });
