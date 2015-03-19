82025e3cd8ac06ac9e33caef0c60b30eb5ea4d3d
Joseph Rosenberg <jofus101@gmail.com>
2015-03-19 11:59:11 -0400

    syntax tweaks to make jshint happy

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -76,26 +76,26 @@ $(function() {
 
       var windowWidth = $window.width(),
             windowHeight = $window.height(),
-            r_w = windowHeight / windowWidth,
-            i_w = img.width,
-            i_h = img.height,
-            r_i = i_h / i_w,
-            new_w, new_h, new_left, new_top;
+            windowRatio = windowHeight / windowWidth,
+            imageWidth = img.width,
+            imageHeight = img.height,
+            imageRatio = imageHeight / imageWidth,
+            newWidth, newHeight; // new_left, new_top;
 
-        if( r_w > r_i ) {
-            new_h   = windowHeight;
-            new_w   = windowHeight / r_i;
+        if( windowRatio > imageRatio ) {
+            newHeight   = windowHeight;
+            newWidth   = windowHeight / imageRatio;
         }
         else {
-            new_h   = windowWidth * r_i;
-            new_w   = windowWidth;
+            newHeight   = windowWidth * imageRatio;
+            newWidth   = windowWidth;
         }
 
         $img.css({
-            width   : new_w,
-            height  : new_h,
-            left    : ( windowWidth - new_w ) / 2,
-            top     : ( windowHeight - new_h ) / 2
+            width   : newWidth,
+            height  : newHeight,
+            left    : ( windowWidth - newWidth ) / 2,
+            top     : ( windowHeight - newHeight ) / 2
         });
     });
   }   
