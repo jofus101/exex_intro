bf14c121030ae8db09b4a16ecdd60a7bc6dadcf4
Joseph Rosenberg <jofus101@gmail.com>
2015-03-23 19:36:23 -0400

    First pass at copying over the email submission form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -154,4 +154,62 @@ $(function() {
   // the rest will fade in on the next button
   fadeInElements();
 
+
+  //################################################
+  // Email submission form
+  //################################################
+  var myFirebaseRef = new Firebase('https://flickering-inferno-9766.firebaseio.com/users/');
+
+
+  function submitForm() {
+    var email = $('#email_input').val().trim();
+
+    if (!email) { return; }
+
+    if (validateEmail(email)) {
+      myFirebaseRef.push({email: email});
+      displaySuccessMessage(email);
+      $('#email_input').val('');
+    }
+    else {
+      displayFailureMessage(email);
+    }
+  }
+
+  function validateEmail(email) { 
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+
+  $('#email_input').keyup(function (event) {
+    var email = $(this).val().trim();
+
+    if (!email) {
+      $(this).removeClass('error success');
+      $('#alert_placeholder').empty();
+    } 
+    else if (validateEmail(email)) {
+      $(this).removeClass('error').addClass('success');
+    }
+    else {
+      $(this).removeClass('success').addClass('error');
+    }
+
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13') {
+      submitForm();    
+    }
+  });
+
+  $('#fire_submit').click(submitForm);
+
+  function displaySuccessMessage(email) {
+    $('#alert_placeholder').empty()
+      .append('Thanks for signing up');
+  }
+  function displayFailureMessage(email) {
+    $('#alert_placeholder').empty()
+      .append('We\'re sorry that doesn\'t look quite right');
+  } 
+
 });
